498b55f044b0a5a3263ccbf7219d04060bdcc9c4
qingda-oicr <34283849+qingda-oicr@users.noreply.github.com>
2018-01-31 14:09:18 -0500

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,6 +1,6 @@
 window.UMS_CONFIG = {
-    APP_ID: '----',
-    API_HOST: '----',
+    APP_ID: 'webdevff0b3218aaad11e7abc4cec278b6b50a_APP_ID',
+    API_HOST: 'webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_DOCKER_PORT',
     CONTACT_LINK: 'mailto:webdev.oicr@gmail.com',
 };
 window.extLinkConfig = {
