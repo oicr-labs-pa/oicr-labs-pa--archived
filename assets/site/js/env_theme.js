a3bc88595a544403e55717aedccae776044f1116
Joseph Yamada <joe.yamada@gmail.com>
2018-10-23 17:04:02 -0400

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,6 +1,6 @@
 window.UMS_CONFIG = {
     APP_ID: '5a2fe819f3bd04c37d00df73',
-    API_HOST: 'api.labs.oicr.on.ca',
+    API_HOST: 'api.nextlabs.oicr.on.ca',
     CONTACT_LINK: 'mailto:webdev.oicr@gmail.com',
 };
 window.extLinkConfig = {
