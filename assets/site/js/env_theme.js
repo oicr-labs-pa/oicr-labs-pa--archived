49547e61362535b67195674642a8f197f9397bec
Joseph Yamada <joe.yamada@gmail.com>
2018-12-06 16:28:59 -0500

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,9 +1,13 @@
-window.UMS_CONFIG = {
+window.APP_CONFIG = {
     APP_ID: '5a2fe819f3bd04c37d00df73',
-    API_HOST: 'api.nextlabs.oicr.on.ca:443',
-    CONTACT_LINK: 'mailto:webdev.oicr@gmail.com',
+    API_HOST: 'api.nextlabs.oicr.on.ca',
+    GITHUB_LOGIN_URL: 'https://api.nextlabs.oicr.on.ca:443/user/auth/github?appid=5a2fe819f3bd04c37d00df73&continue=https%3A%2F%2Fpawadallalab.org%2Fuser%2F%23%2Fdashboard',
+    CONTACT_LINK: 'webdev@oicr.on.ca',
+    CMUI_ENABLED: true,
+    FORM_ENABLED: false,
+    FROALA_ACTIVATION_KEY: 'bJ3B10B7D7cD3F3C2C1G3C5C3B6E2E2vdenC-22A8E-13E-13B1pu==',
 };
 window.extLinkConfig = {
     extAlert: '_Alert',
-    extClass: 'ext',
+    extClass: 'ext'
 };
